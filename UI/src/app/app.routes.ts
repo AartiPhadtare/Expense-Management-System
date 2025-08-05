1c77c45e4a16dfa521f318de51af3f3c16f5260d
Aarti <aarti@support.unlocktech.in>
2025-08-05 08:54:41 -0500

    initial commit

---
+++ UI/src/app/app.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from '@angular/router';
+import { Login } from './components/login/login';
+import { Register } from './components/Register/register';
+
+export const routes: Routes = [
+    {
+        path:'',
+        redirectTo:'login',
+        pathMatch:'full'
+    },
+    {
+        path:'login',
+        component:Login
+    },
+    {
+        path:'register',
+        component:Register
+    }
+];
